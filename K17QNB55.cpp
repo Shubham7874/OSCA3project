f4aa9186ee835b512316b87f0350cfd1ea276b01
Shubham Thakur <38554823+Shubham7874@users.noreply.github.com>
2019-04-01 12:31:46 +0530

    Add files via upload

---
+++ K17QNB55.cpp
@@ -0,0 +1,200 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+void input(int All[][10],int Need[][10],int Max[10][10],int n,int r)
+{
+        for(int i=0;i<n;i++){
+		printf("\n Processes %d\n",i);
+		for(int j=0;j<r;j++){
+			printf(" Allocation for resource %d : ",j+1);
+			scanf("%d",&All[i][j]);
+			}
+	   }
+        for(int i=0;i<n;i++){
+		printf("\n Process %d\n",i);
+		for(int j=0;j<r;j++){
+			printf(" Max for resource %d : ",j+1);
+			scanf("%d",&Max[i][j]);
+		}
+	}
+		for(int i=0;i<n;i++)
+		for(int j=0;j<r;j++)
+			Need[i][j]=Max[i][j]-All[i][j];
+	printf("\n ");
+	printf("Allocation Matrix Will Be");
+	for(int i=0;i<n;i++)
+	{
+		printf("\n");
+		for(int j=0;j<r;j++)
+		{
+			printf("%d\t",All[i][j]);
+		}
+	}
+	printf("\n ");
+	printf("Max Requirement Matrix Will Be");
+	for(int i=0;i<n;i++)
+	{
+		printf("\n");
+		for(int j=0;j<r;j++)
+		{
+			printf("%d\t",Max[i][j]);
+		}
+	}
+	printf("\n ");
+	printf("Needed Matrix");
+	for(int i=0;i<n;i++)
+	{
+		printf("\n");
+		for(int j=0;j<r;j++)
+		{
+			printf("%d\t",Need[i][j]);
+		}
+	}
+}
+
+int checks(int Need[10][10],int Avail[1][10],int m,int r)
+{
+for(int i=m;i<m+1;i++){
+  for(int j=0;j<r;j++)
+      {
+
+       if(Need[i][j]>Avail[0][j])
+            {return 0;}
+    }}
+return 1;
+}
+
+int checks1(int Need[10][10],int Avail1[1][10],int m,int r)
+{
+for(int i=m;i<m+1;i++){
+  for(int j=0;j<r;j++)
+      {
+
+       if(Need[i][j]>Avail1[0][j])
+            {return 0;}
+}}
+return 1;
+}
+int checks2(int req[1][10],int Need[10][10],int p,int r)
+{
+  for(int j=0;j<r;j++)
+      {
+
+       if(req[0][j]>Need[p][j])
+            {return 0;}
+
+}
+return 1;
+}
+
+void request(int All[10][10],int Need[10][10],int Avail1[10][10],int n,int r)
+{
+printf("Enter process number");
+int p;
+int req[1][10];
+scanf("%d",&p);
+printf("Enter the resources:");
+for(int j=0;j<r;j++)
+{
+  scanf("%d",&req[0][j]);}
+  if(checks2(req,Need,p,r)==1)
+{
+for(int j=0;j<r;j++)
+{
+Avail1[0][j]=Avail1[0][j]-req[0][j];
+}
+for(int j=0;j<r;j++)
+{
+All[p][j]=Avail1[0][j]+req[0][j];
+}
+for(int j=0;j<r;j++)
+{
+Need[p][j]=Need[p][j]-req[0][j];
+}
+
+int done1[n];
+int s=0;
+while(s<n)
+{
+int allocated1=0;
+for(int i=0;i<n;i++)
+if(done1[i]==0 && checks1(Need,Avail1,i,r)==1){
+    for(int j=0;j<r;j++)
+       Avail1[0][j]=Avail1[0][j]+All[i][j];
+ printf("\n Allocated process will be %d ",i);
+
+       done1[i]=1;
+       allocated1=1;
+       s++;
+   }
+if(allocated1==0)break;
+}
+
+if(s==n)
+{
+printf("\n SAFE");
+}
+else{
+printf("\n UNSAFE");
+exit(0);}
+}
+}
+int safe(int All[10][10],int Need[10][10],int Max[10][10],int Avail[1][10],int n,int r)
+{
+int done[n];
+int c=0;
+while(c<n)
+{
+int allocated=0;
+for(int i=0;i<n;i++)
+if(done[i]==0 && checks(Need,Avail,i,r)==1){
+    for(int j=0;j<r;j++)
+       Avail[0][j]=Avail[0][j]+All[i][j];
+ printf("\n Allocated process %d ",i);
+
+       done[i]=1;
+       allocated=1;
+       c++;
+   }
+if(allocated==0)break;
+}
+if(c==n)
+{
+	printf("\n SAFE");
+}
+
+else{
+printf("\n UNSAFE");
+return 0;}
+}
+int main()
+{  int All[10][10];
+	int Max[10][10];
+	int Need[10][10];
+	int Avail[1][10];
+	int Avail1[1][10];
+	int n,r,ret,ch;
+	printf("\n Enter total no. of Processes : ");
+	scanf("%d",&n);
+	printf("\n Enter total no. of Resources : ");
+	scanf("%d",&r);
+	input(All,Need,Max,n,r);
+	printf("\n Available resources will be : \n");
+	for(int i=0;i<r;i++){
+		printf(" Resource %d : ",i+1);
+		scanf("%d",&Avail[0][i]);
+		Avail1[0][i]=Avail[0][i];}
+	ret=safe(All,Need,Max,Avail,n,r);
+	if(ret!=0)
+	{
+		 printf("\n Do you want make additional requests ? (1=Yes|0=No)");
+		scanf("%d",&ch);
+		if(ch == 1){
+
+			request(All,Need,Avail1,n,r);
+			if(ret == 0 )
+				exit(0);
+		}
+	}else
+		exit(0);
+	}
